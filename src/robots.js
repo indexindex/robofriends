b2328bbadd473567fe072bca377d69d30b880c58
zippelgazz <46674488+indexindex@users.noreply.github.com>
2020-06-29 12:13:27 +0300

    adding project files

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'robot 1',
+        username: 'Arobot',
+        email: 'robot1@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'robot 2',
+        username: 'Brobot',
+        email: 'robot2@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'robot 3',
+        username: 'Crobot',
+        email: 'robot3@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'robot 4',
+        username: 'Drobot',
+        email: 'robot4@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'robot 5',
+        username: 'Erobot',
+        email: 'robot5@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'robot 6',
+        username: 'Frobot',
+        email: 'robot6@gmail.com'
+    }
+];
