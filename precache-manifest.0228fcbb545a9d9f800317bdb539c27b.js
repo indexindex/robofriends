023403f2b5bb2153f2df9d65f716cd30cf3173c7
zippelgazz <46674488+indexindex@users.noreply.github.com>
2020-06-29 12:20:55 +0300

    Updates

---
+++ precache-manifest.0228fcbb545a9d9f800317bdb539c27b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525c0de5a5e1d9b5d48848a259e92fbf",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fd8fec93fa2866c45480",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "759a7cbbeef1260896b3",
+    "url": "/robofriends/static/css/main.c717c83d.chunk.css"
+  },
+  {
+    "revision": "fd8fec93fa2866c45480",
+    "url": "/robofriends/static/js/2.1a5b6f8d.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robofriends/static/js/2.1a5b6f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759a7cbbeef1260896b3",
+    "url": "/robofriends/static/js/main.d8366851.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "3efcfc57d21fe335dca00353f62d7ac9",
+    "url": "/robofriends/static/media/Robots.3efcfc57.otf"
+  }
+]);
